50dd834d69354952f1fbcb72d2db9a85d580dd97
Hung-Yang-Shih <d11416204@gs.takming.edu.tw>
2026-03-13 12:07:27 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _0313_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test joe, mary, tom;
+
+            joe = new Test();
+            mary = new Test();
+
+            joe.SetGrade(70, 80);
+            mary.SetGrade(70, 90);
+
+            Console.WriteLine(joe.GetAvg());
+           
+            Console.WriteLine(mary.GetAvg());
+
+
+        }
+    }
+}
